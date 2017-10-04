b4ac47247e29547ae702f661d35a383306c3fc3b
anathik <me@anathi.io>
2017-10-03 20:56:28 -0400

    First Commit

---
+++ org.quickstack.backend.core/src/org/quickstack/core/api/service/IBaseService.java
@@ -0,0 +1,7 @@
+package org.quickstack.core.api.service;
+
+import org.quickstack.core.api.ApplicationException;
+
+public interface IBaseService {
+	public void createDefaults() throws ApplicationException;
+}
